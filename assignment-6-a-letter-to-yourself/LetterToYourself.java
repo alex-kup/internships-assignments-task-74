016af4cb4f0a0a0f017b679980d630a79652c40a
Alex Kuptsov <alex.kuptcov@gmail.com>
2016-03-16 22:04:32 +0300

    five-six

---
+++ assignment-6-a-letter-to-yourself/LetterToYourself.java
@@ -0,0 +1,17 @@
+public class LetterToYourself
+{
+          public static void main( String[] args )
+          {
+                     System.out.println( "+---------------------------------------------------------+" );
+                     System.out.println( "|                                                    #### |" );
+                     System.out.println( "|                                                    #### |" );
+                     System.out.println( "|                                                    #### |" );
+                     System.out.println( "|                                                         |" );
+                     System.out.println( "|                                                         |" );
+                     System.out.println( "|                              Alex Kuptcov               |" );
+                     System.out.println( "|                              1 Mira street              |" );
+                     System.out.println( "|                              Moscow, MO 11111           |" );
+                     System.out.println( "|                                                         |" );
+                     System.out.println( "+---------------------------------------------------------+" );
+           }
+}
